e9fcf4dc8bb885f6b1090cef703954f82bb1f614
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 20:37:43 +0530

    added classes and methods PingPongPaddles

--- src/PingPongPaddles.java
+++ src/PingPongPaddles.java
@@ -1,7 +1,35 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class PingPongPaddles {
 
     // creating a constructor
     PingPongPaddles() {
+
+    }
+
+    // method when the key is pressed
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    // method when the key is released
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    // method to set the direction of Y axis
+    public void setYDirection(int yDirection) {
+
+    }
+
+    // method to move the paddles
+    public void move() {
+
+    }
+
+    // method to draw the paddles
+    public void draw(Graphics g) {
 
     }
 
