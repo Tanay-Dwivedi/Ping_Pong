2e5ad6d58146bbe585f685738465c082124121b3
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 20:47:33 +0530

    added methods in PingPongScore

--- src/PingPongScore.java
+++ src/PingPongScore.java
@@ -1,7 +1,14 @@
+import java.awt.*;
+
 public class PingPongScore {
 
     // creating a constructor
     PingPongScore() {
+
+    }
+
+    // method to show the score
+    public void draw(Graphics g) {
 
     }
 
