ab5bed7cb6e5430b07ea189a71deca9334b7585e
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 21:32:09 +0530

    added the frame properties

--- src/PingPongFrame.java
+++ src/PingPongFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PingPongFrame extends JFrame {
 
@@ -7,6 +8,17 @@ public class PingPongFrame extends JFrame {
 
     // creating a constructor
     PingPongFrame() {
+
+        // adding the frame properties
+        pingPongPanel = new PingPongPanel();
+        this.add(pingPongPanel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
 
     }
 
