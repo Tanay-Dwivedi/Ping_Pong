ecd51c41f333287703c7062f2ba1c3454253bff3
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 20:46:09 +0530

    added methods in PongBall

--- src/PongBall.java
+++ src/PongBall.java
@@ -1,7 +1,29 @@
+import java.awt.*;
+
 public class PongBall {
 
     // creating a constructor
     PongBall() {
+
+    }
+
+    // method to set the direction of X axis
+    public void setXDirection(int randomXDirection) {
+
+    }
+
+    // method to set the direction of Y axis
+    public void setYDirection(int randomYDirection) {
+
+    }
+
+    // method to move the ball
+    public void move() {
+
+    }
+
+    // method to draw the ball
+    public void draw(Graphics g) {
 
     }
 
