3ff5f42cd838928d6307f3c7b33e2642fd7e4b8b
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 23:44:40 +0530

    added the draw method for the score panel

--- src/PingPongScore.java
+++ src/PingPongScore.java
@@ -19,6 +19,14 @@ public class PingPongScore extends Rectangle {
     // method to show the score
     public void draw(Graphics g) {
 
+        g.setColor(Color.white);
+        g.setFont(new Font("Consolas",Font.PLAIN,60));
+
+        g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+
+        g.drawString(playerA / 10 +String.valueOf(playerA%10), (GAME_WIDTH/2)-85, 50);
+        g.drawString(playerB / 10 +String.valueOf(playerB%10), (GAME_WIDTH/2)+20, 50);
+
     }
 
 }
