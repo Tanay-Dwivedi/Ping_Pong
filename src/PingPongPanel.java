9653134e84907d1f1b84eff396155706e9b38959
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-10 23:02:35 +0530

    done with run method of the panel

--- src/PingPongPanel.java
+++ src/PingPongPanel.java
@@ -78,6 +78,23 @@ public class PingPongPanel extends JPanel implements Runnable{
     // method to run the game
     public void run() {
 
+        // creating the game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks =60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now -lastTime)/ns;
+            lastTime = now;
+            if(delta >=1) {
+                move();
+                collision();
+                repaint();
+                delta--;
+            }
+        }
+
     }
 
     // class for the action listener
